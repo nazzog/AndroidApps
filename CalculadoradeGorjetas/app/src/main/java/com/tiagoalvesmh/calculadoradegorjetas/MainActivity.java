b3b229be3118ccece717e44f905f5231206a57e9
Tiago Mohnschmidt <tiagoalvesmh@gmail.com>
2019-04-12 17:04:27 -0300

    SeekBar implementado

--- CalculadoradeGorjetas/app/src/main/java/com/tiagoalvesmh/calculadoradegorjetas/MainActivity.java
+++ CalculadoradeGorjetas/app/src/main/java/com/tiagoalvesmh/calculadoradegorjetas/MainActivity.java
@@ -2,12 +2,45 @@ package com.tiagoalvesmh.calculadoradegorjetas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText et_valor;
+    private TextView tv_porcentagem;
+    private TextView tv_gorjeta;
+    private TextView tv_total;
+    private SeekBar sk_gorjeta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_valor = findViewById(R.id.etValor);
+        tv_porcentagem = findViewById(R.id.tvPorcentagem);
+        tv_gorjeta = findViewById(R.id.tvGorjeta);
+        tv_total = findViewById(R.id.tvTotal);
+        sk_gorjeta = findViewById(R.id.skGorjeta);
+
+        sk_gorjeta.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int porcentagem = seekBar.getProgress();
+                tv_porcentagem.setText(porcentagem + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
