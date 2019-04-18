5ea08f2cf6d8d7997aab35f3ccae8e4abccffd43
Tiago Mohnschmidt <tiagoalvesmh@gmail.com>
2019-04-18 16:13:55 -0300

    CheckBoxes implementados.

--- TesteDeComponentes/app/src/main/java/com/tiagoalvesmh/testedecomponentes/ButtonsActivity.java
+++ TesteDeComponentes/app/src/main/java/com/tiagoalvesmh/testedecomponentes/ButtonsActivity.java
@@ -6,14 +6,19 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ButtonsActivity extends AppCompatActivity {
 
     //Declarando Componentes
     private EditText edit_text_nome;
+    private RadioGroup sexo;
     private RadioButton radio_masc;
     private RadioButton radio_fem;
     private CheckBox box_por;
@@ -29,12 +34,17 @@ public class ButtonsActivity extends AppCompatActivity {
     private TextView percentResposta;
     private TextView resposta;
 
+
+    //Outras variaveis
+    List<String> cb_list = new ArrayList<String>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buttons);
 
         edit_text_nome = findViewById(R.id.editTextNome);
+        sexo = findViewById(R.id.radioGroupSexo);
         radio_masc = findViewById(R.id.radioMasculino);
         radio_fem = findViewById(R.id.radioFeminino);
         box_por = findViewById(R.id.checkBoxPor);
@@ -52,7 +62,48 @@ public class ButtonsActivity extends AppCompatActivity {
     }
 
     public void Gerar(View view){
+        //Recupera o nome digitado.
         String nome = edit_text_nome.getText().toString();
-        resposta.setText(nome + ", do sexo "+", fala os idiomas: ");
+
+        //Verifica RadioButtons
+        verificaRadio();
+
+        //Método para verificação do CheckBox.
+        verificaCheck();
+
+        resposta.setText(nome + ", do sexo "+", fala os idiomas: " + cb_list);
     }
+
+    public void verificaCheck(){
+        cb_list.clear();
+
+        if(box_por.isChecked()){
+            cb_list.add(box_por.getText().toString());
+        }
+        if(box_ing.isChecked()){
+            cb_list.add(box_ing.getText().toString());
+        }
+        if(box_esp.isChecked()){
+            cb_list.add(box_esp.getText().toString());
+        }
+        if(box_fra.isChecked()){
+            cb_list.add(box_fra.getText().toString());
+        }
+        if(box_ale.isChecked()){
+            cb_list.add(box_ale.getText().toString());
+        }
+        if(box_ita.isChecked()){
+            cb_list.add(box_ita.getText().toString());
+        }
+    }
+
+    public void verificaRadio(){
+        sexo.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                
+            }
+        });
+    }
+
 }
