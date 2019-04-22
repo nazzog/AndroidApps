a1a183ffa8c7f11aefc94072663f4ab4a2906f89
Tiago Mohnschmidt <tiagoalvesmh@gmail.com>
2019-04-22 16:44:36 -0300

    Activity RecyclerView, classe Adapter e layout do RecyclerView add.

--- TesteDeComponentes/app/src/main/java/com/tiagoalvesmh/testedecomponentes/RecyclerActivity.java
+++ TesteDeComponentes/app/src/main/java/com/tiagoalvesmh/testedecomponentes/RecyclerActivity.java
@@ -2,12 +2,27 @@ package com.tiagoalvesmh.testedecomponentes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class RecyclerActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        //Adapter
+
+
+        //Conf RecyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        //recyclerView.setAdapter();
     }
 }
---
+++ TesteDeComponentes/app/src/main/java/com/tiagoalvesmh/testedecomponentes/adapter/Adapter.java
@@ -0,0 +1,39 @@
+package com.tiagoalvesmh.testedecomponentes.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
+
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView titulo;
+        TextView ano;
+        TextView genero;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+        }
+    }
+}
